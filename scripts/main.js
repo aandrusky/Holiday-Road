c20d21589b7fd71f7378799f134328e7403c314e
dtracydepp <tracydepp82@gmail.com>
2020-10-29 15:09:10 -0500

    updated eaterieslist.js and eatery.js to correctly render to the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
-import { ParkSelect } from "./parks/ParkSelect.js";
-import { ParkList } from "./parks/ParkList.js";
+// import { ParkSelect } from "./parks/ParkSelect.js";
+// import { ParkList } from "./parks/ParkList.js";
+import { getEateries, useEateries } from "./eateries/EateryProvider.js";
+import { EateriesSelect } from "./eateries/EateriesSelector.js";
+import { Eateries } from "./eateries/Eateries.js";
+import { EateriesList } from "./eateries/EateriesList.js";
 
-ParkSelect()
+// ParkSelect()
 // ParkList()
+
+getEateries()
+useEateries()
+EateriesSelect()
+
