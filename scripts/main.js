39c7b1db96fe760929828297af9a6d01ca73f51f
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-11-02 09:20:37 -0600

    copleted the dropdown menu, did basic css.

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+console.log("We are Organ-pipe-cactus!!!")
+
+import {useAttractions} from "./attractions/AttractionProvider.js"
+import {getAttractions} from "./attractions/AttractionProvider.js"
+import {Attraction} from "./attractions/AttractionHTML.js"
+import {AttractionList} from "./attractions/AttractionList.js"
+import {AttractionSelect} from "./attractions/AttractionSelect.js"
+// import  "./attractions/AttractionDetail.js"
+import {AttractionDetails} from "./attractions/AttractionDetail.js"
+
+
+
+
+// AttractionList()
+AttractionSelect()
+AttractionDetails()
+
